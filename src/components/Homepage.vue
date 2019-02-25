cb6bba8179328598a8329d28b483e6d083fae16d
Sergio Fruto <fruto.sergio@gmail.com>
2019-02-25 00:08:48 -0300

    color palette

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -7,12 +7,14 @@
         <p>I'm a web developer located in BA, currently working for Auth0.</p>
         <p>I make websites look good and behave nice in every browser and device.</p>
         <p>I have 6 years of experience in web development, focusing in HTML, CSS and some help of JS, following the modern standards for Responsive Web Design and Mobile First.</p>
-        <button
-          v-bind:style="{ color: white, border: `1px solid ${currentColor}`}"
+        <div
+          :style="style" class="color-palette"
           v-on:click="updateColor"
         >
-          update color!
-        </button>
+          <span class="color-circle color-1"></span>
+          <span class="color-circle color-2"></span>
+          <span class="color-circle color-3"></span>
+        </div>
       </div>
     </div>
     <span v-bind:style="{ backgroundColor: currentColor }" class="bar bottom-bar"></span>
@@ -31,24 +33,29 @@ export default {
     return {
       startingColor: 0,
       currentColor: '#11cbdc',
+      currentRotation: 60,
     };
+  },
+  computed: {
+    style () {
+      return { transform: 'rotate(' + this.currentRotation + 'deg)'}
+    }
   },
   methods: {
     updateColor: function () {
-      const colors = ['#11cbdc', '#FDB52B', '#E0292D', '#FFFFFF'];
-      if (this.startingColor < 3) {
+      const colors = ['#11cbdc', '#FDB52B', '#E0292D'];
+      if (this.startingColor < 2) {
         this.startingColor = this.startingColor + 1;
+        this.currentRotation = this.currentRotation - 120;
       }
       else {
         this.startingColor = 0;
+        this.currentRotation = 60;
       }
 
       this.currentColor = colors[this.startingColor];
     }
-  },
-  mounted: function () {
-    this.updateColor();
-  },
+  }
 };
 
 
@@ -88,6 +95,49 @@ span.bar {
   height: 4.5rem;
   background-color: black;
   transition: all 1s ease;
+}
+
+.color-palette {
+  position: relative;
+  display: flex;
+  width: 80px;
+  height: 75px;
+  margin-top: 40px;
+  transform-origin: center center;
+  transition: all .35s ease-in;
+}
+
+.color-palette:hover,
+.color-palette:focus {
+  cursor: pointer;
+}
+
+span.color-circle {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+}
+
+.color-1 {
+  position: absolute;
+  top:0;
+  left: 0;
+  background-color: #11cbdc;
+}
+
+.color-2 {
+  position: absolute;
+  top:0;
+  left: 30px;
+  background-color: #FDB52B;
+}
+
+.color-3 {
+  position: absolute;
+  top: 25px;
+  left: 15px;
+  background-color: #E0292D;
 }
 
 button {
