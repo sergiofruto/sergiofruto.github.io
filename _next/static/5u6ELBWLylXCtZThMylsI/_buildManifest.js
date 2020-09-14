84878fef8d3d8862e7415520f985cca543c6b30c
Sergio <fruto.sergio@gmail.com>
2020-09-14 02:25:36 -0300

    plain version

--- _next/static/5u6ELBWLylXCtZThMylsI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F68d022cbd74fb20418a6.css","static\u002Fchunks\u002Fpages\u002Findex-62ed878ad63684621eef.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
